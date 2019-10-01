cbdb693c99d1086c95a594333f9cb98b364d46ac
JHodor <jedrzej.hodor@gmail.com>
2019-10-01 11:39:47 +0200

    fixed readme

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-cd /home/hodor/Flash/shade/
-java --module-path  $JAVA_HOME/javafx-sdk-11.0.2/lib/ --add-modules=javafx.controls,javafx.fxml --add-exports=javafx.base/com.sun.javafx.event=ALL-UNNAMED -jar /home/hodor/Flash/shade/Flash.jar
-cd -
+java --module-path  $JAVA_HOME/javafx-sdk-11.0.2/lib/ --add-modules=javafx.controls,javafx.fxml --add-exports=javafx.base/com.sun.javafx.event=ALL-UNNAMED -jar Flash.jar
